a6226fdb45abbb08c2b60045e2400217114de5b7
tellusaflame <141950251+tellusaflame@users.noreply.github.com>
2024-09-15 23:14:58 +0300

    Create update_system.sh

---
+++ scripts/update_system.sh
@@ -0,0 +1,9 @@
+run_command() {
+    eval "$1 &>> setup_script.log"  # Выполняем команду и скрываем вывод
+    # echo "Нажмите любую клавишу для продолжения..."
+    # read -n 1 -s  # Ожидаем нажатия клавиши
+    # sleep 1  # Пауза на 1 секунду
+}
+
+echo "Updating system components..."
+run_command "sudo apt update -qq && sudo apt upgrade -qq -y"
